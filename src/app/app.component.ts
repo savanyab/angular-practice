9bc469f8e40e45b040d7cee17b733f1fa8d74e93
savanyab <savanyab@gmail.com>
2018-12-14 08:54:48 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-course4';
+  name = '';
+
+  greeter() {
+    alert("Hello " + this.name);
+  }
+
+  getBg() {
+    if (this.name[0] === 'S' || this.name[0] === 's') {
+      return "red";
+    } else if (this.name.length > 3) {
+      return "blue";
+    } else {
+      return "black";
+    }
+  }
+
+  getFontColor() {
+    if (this.name[0] === 'S' || this.name[0] === 's') {
+      return "green";
+    } else if (this.name.length > 3) {
+      return "yellow";
+    }else {
+      return "white";
+    }
+  }
+
+  getSize() {    
+      return '60px';    
+  }
 }
